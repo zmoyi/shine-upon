8057b4d9f8cfbed16ca667b6ddfa605ad68577ce
MingXiLiu <zmszm666@163.com>
2023-12-14 17:33:35 +0800

    fix: 修正 index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,31 +18,35 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
+        <html lang="zh">
         <body className={inter.className}>
         <header
-            className={'flex flex-row justify-between px-4 py-5 bg-neutral-900/90 text-white font-serif rounded-b-sm sticky top-0'}>
+            className={'flex flex-row justify-between items-center px-4 py-5 bg-neutral-100/90 dark:bg-neutral-900/90 text-white rounded-b-lg sticky top-0'}>
             <div>
-                <h6 className={'font-serif'}>index</h6>
+                <h6 className={'text-black dark:text-white font-serif'}>index</h6>
             </div>
-            <div className={'text-black w-16 bg-stone-50 rounded-full text-sm flex justify-center items-center'}>
+            <div className={'w-24 h-8 text-white dark:text-black bg-neutral-900/90 dark:bg-stone-50 rounded-full text-sm flex justify-center items-center'}>
                 login
             </div>
         </header>
-        <main className={'mt-4 font-serif'}>
-            <div className={'mx-4 px-4 py-4 bg-neutral-900/90 rounded-sm flex flex-row justify-end items-start'}>
+        <main className={'mt-4'}>
+            <div className={"m-4 font-bold"}>
+                Is Me
+            </div>
+            <div
+                className={'m-4 p-4 bg-neutral-100/90 dark:bg-neutral-900/90 rounded-lg flex flex-row justify-end items-start'}>
                 <div className={'flex flex-row'}>
                     <div className={'flex flex-col justify-end items-end mr-2'}>
-                        <h1 className={'text-sm mb-1'}>刘铭熙</h1>
-                        <h6 className={'text-xs font-light'}>我愿在这星球上种一朵玫瑰花，看她花开花落.</h6>
+                        <h1 className={'text-base'}>刘铭熙</h1>
+                        <h6 className={'text-sm font-light'}>我愿在这星球上种一朵玫瑰花，看她花开花落.</h6>
                     </div>
-                    <Image className={'h-12 w-12 rounded-lg'} src={WechatImg} alt={'刘铭熙'}/>
+                    <Image className={'h-16 w-16 rounded-lg'} src={WechatImg} alt={'刘铭熙'}/>
                 </div>
             </div>
             {children}
         </main>
         <footer>
-            <div className={'flex uppercase justify-center items-center px-4 py-5 text-white text-xs font-light'}>
+            <div className={'flex uppercase justify-center items-center px-4 py-5 dark:text-white text-xs font-light'}>
                 © 2023 SOLITUDE 随欲. All Right Reserved.
             </div>
         </footer>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,104 @@
 import Image from "next/image";
-import {IconDots, IconHeart, IconMessage2, IconRefresh} from "@tabler/icons-react";
+import {IconDots, IconHeart, IconMessage, IconRefresh} from "@tabler/icons-react";
 import {postData} from "../../lib/data";
 export default function Home() {
 
     return (
-        <>{
+        <>
+            <div className={"m-4 font-bold"}>
+                All posts
+            </div>
+            {
             postData.map((item, index)=>{
                 return (
-                    <div key={index} className={'my-4 mx-4 px-4 py-4 bg-neutral-900/90 rounded-sm'}>
+                    <div key={index} className={'m-4 p-4 bg-neutral-100/90 dark:bg-neutral-900/90 rounded-lg'}>
                         <div className={'flex flex-row justify-start items-center'}>
-                            <Image className={'w-11 h-11 rounded-full'} src={item.avatar} alt={'刘铭熙'}/>
+                            <Image className={'w-14 h-14 rounded-full'} src={item.avatar} alt={'刘铭熙'}/>
                             <div className={'ml-2'}>
-                                <h6 className={'text-sm font-bold'}>{item.name}</h6>
+                                <h6 className={'text-base font-bold'}>{item.name}</h6>
                                 <p className={'text-xs font-light'}>@ {item.username}</p>
                             </div>
                         </div>
-                        <p className={'text-justify my-2 indent-8 text-sm font-light leading-relaxed'}>
+                        <p className={'text-justify my-2 indent-8 text-sm font-normal leading-7 antialiased line-clamp-4'}>
                             {item.content}
                         </p>
                         {
                             item.img && <Image className={'rounded-lg'} src={item.img} alt={'bg'}/>
                         }
-                        <p className={'text-xs font-black my-2'}>{item.time} · {item.likes} Likes</p>
-                        <div className={'flex flex-row mt-3'}>
-                            <div className={'basis-2/3 flex flex-row justify-between items-center'}>
-                                <IconMessage2/>
-                                <IconRefresh/>
-                                <IconHeart/>
-                                <IconDots/>
+                        <div className={'mt-4 bg-neutral-300/30 dark:bg-neutral-800/90 rounded-lg py-2 px-4'}>
+                            <p className={'text-xs font-black my-2'}>{item.time} · {item.likes} Likes</p>
+                            <div
+                                className={'flex flex-row justify-between items-center my-2'}>
+
+                                <div className={'flex flex-row justify-start items-center'}>
+                                    <IconRefresh size={20}/>
+                                    <p className={'text-sm ml-1 font-normal'}>10</p>
+                                </div>
+                                <div className={'flex flex-row justify-start items-center'}>
+                                    <IconHeart size={20}/>
+                                    <p className={'text-sm ml-1 font-normal'}>1</p>
+                                </div>
+                                <IconDots size={20}/>
+                            </div>
+                        </div>
+
+                        <div className={'mt-4 bg-neutral-300/20 dark:bg-neutral-800/90 rounded-lg p-4'}>
+                            <div className={'flex flex-row justify-between items-center'}>
+                                <div className={'flex flex-row justify-start items-center'}>
+                                    <IconMessage size={20} className={'mr-2'}/>
+                                    <p className={'text-sm font-bold'}>
+                                        Comments
+                                    </p>
+                                </div>
+                                <div
+                                    className={'w-24 h-8 text-white dark:text-black bg-neutral-900/90 dark:bg-stone-50 rounded-full text-sm flex justify-center items-center'}>
+                                    评论
+                                </div>
+                            </div>
+                            <div className={'my-4 bg-neutral-300/50 dark:bg-neutral-700/20 rounded-lg py-2 px-4'}>
+                                <div className={'flex flex-row justify-start items-start my-2'}>
+                                    <Image className={'w-10 h-10 rounded-full'} src={item.avatar} alt={'刘铭熙'}/>
+                                    <div className={'ml-2'}>
+                                        <div className={'flex flex-row justify-between items-center'}>
+                                            <h6 className={'text-base font-bold'}>{item.name}</h6>
+                                            <p className={'text-sm font-bold'}>回复</p>
+                                        </div>
+                                        <p className={'text-xs font-light mb-1'}>回复：@LiuMingXi</p>
+                                        <p className={'text-xs leading-5'}>
+                                            Tailwind 允许您使用变体修饰符有条件地在不同状态下应用实用程序类。例如，用于仅在悬停时应用该实用程序
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={'my-4 bg-neutral-300/50 dark:bg-neutral-700/20 rounded-lg py-2 px-4'}>
+                                <div className={'flex flex-row justify-start items-start my-2'}>
+                                    <Image className={'w-10 h-10 rounded-full'} src={item.avatar} alt={'刘铭熙'}/>
+                                    <div className={'ml-2'}>
+                                        <h6 className={'text-base font-bold'}>{item.name}</h6>
+                                        <p className={'text-xs font-light mb-1'}>回复：@LiuMingXi</p>
+                                        <p className={'text-xs leading-5'}>
+                                            Tailwind 允许您使用变体修饰符有条件地在不同状态下应用实用程序类。例如，用于仅在悬停时应用该实用程序
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={'my-4 bg-neutral-300/50 dark:bg-neutral-700/20 rounded-lg py-2 px-4'}>
+                                <div className={'flex flex-row justify-start items-start my-2'}>
+                                    <Image className={'w-10 h-10 rounded-full'} src={item.avatar} alt={'刘铭熙'}/>
+                                    <div className={'ml-2'}>
+                                        <h6 className={'text-base font-bold'}>{item.name}</h6>
+                                        <p className={'text-xs font-light mb-1'}>回复：@LiuMingXi</p>
+                                        <p className={'text-xs leading-5'}>
+                                            Tailwind 允许您使用变体修饰符有条件地在不同状态下应用实用程序类。例如，用于仅在悬停时应用该实用程序
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 )
             })
-        }
+            }
         </>
     )
 }
